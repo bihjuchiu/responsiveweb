65fe76a45d8b019d31814d89e18f8856f67ee389
bihju <beatriceguardregister@gmail.com>
2015-12-07 08:49:48 -0800

    add sorting ability

--- js/controller/listCtrl.js
+++ js/controller/listCtrl.js
@@ -5,6 +5,18 @@ ListCtrl = function($scope) {
   this.scope = $scope;
   this.title = "List items";
   this.listData = [
+       {
+     	  time: 'Jan 3, 2014 12:30 PM PST8PDT',
+     	  item: 'Item 3',
+     	  owner: 'Owner 3',
+     	  size: 30
+       },
+       {
+     	  time: 'Jan 4, 2014 12:30 PM PST8PDT',
+    	  item: 'Item 4',
+    	  owner: 'Owner 4',
+    	  size: 40
+      },
       {
     	  time: 'Jan 1, 2014 12:30 PM PST8PDT',
     	  item: 'Item 1',
@@ -16,18 +28,6 @@ ListCtrl = function($scope) {
     	  item: 'Item 2',
     	  owner: 'Owner 2',
     	  size: 20
-      },
-      {
-    	  time: 'Jan 3, 2014 12:30 PM PST8PDT',
-    	  item: 'Item 3',
-    	  owner: 'Owner 3',
-    	  size: 30
-      },
-      {
-    	  time: 'Jan 4, 2014 12:30 PM PST8PDT',
-    	  item: 'Item 4',
-    	  owner: 'Owner 4',
-    	  size: 40
       },
       {
     	  time: 'Jan 5, 2014 12:30 PM PST8PDT',
@@ -42,5 +42,41 @@ ListCtrl = function($scope) {
     	  size: 60
       }
   ];
+  
 };
+
+ListCtrl.prototype.reverse = true;
+
+ListCtrl.prototype.sortByTime = function() {
+  var currentOrder = this.reverse;
+  this.listData.sort(function(a, b) {
+    return (currentOrder ?
+	    b.time.localeCompare(a.time) : a.time.localeCompare(b.time));
+  });
+};
+
+ListCtrl.prototype.sortByItem = function() {
+  var currentOrder = this.reverse;
+  this.listData.sort(function(a, b) {
+	  return (currentOrder ?
+	    b.item.localeCompare(a.item) : a.item.localeCompare(b.item));
+  });
+};
+
+ListCtrl.prototype.sortByOwner = function() {
+  var currentOrder = this.reverse;
+  this.listData.sort(function(a, b) {
+	  return (currentOrder ?
+			    b.owner.localeCompare(a.owner) : a.owner.localeCompare(b.owner));
+  });
+};
+
+ListCtrl.prototype.sortBySize = function() {
+  var currentOrder = this.reverse;
+  this.listData.sort(function(a, b) {
+	  return (currentOrder ?
+			    b.size - a.size : a.size - b.size);
+  });
+};
+
 
